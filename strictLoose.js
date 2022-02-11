316e42a5a3d440a51f39eba5bfdd14ed5a5bad64
Divyashree356 <dshagrawal13@gmail.com>
2022-02-11 13:29:27 +0530

    double equals and tripple equals

---
+++ strictLoose.js
@@ -0,0 +1,12 @@
+let a=2;  //integer
+
+
+let  b='2'; //string
+
+
+//loose checking
+console.log(a==b);  //output -true 
+//double eqauls does not check data type it only check the data
+
+//strict checking
+console.log(a===b)  //it checks datatype also
