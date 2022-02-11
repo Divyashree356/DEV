ef4790767ff16bf095280cc0a0fd8faf4f6a892d
Divyashree356 <dshagrawal13@gmail.com>
2022-02-11 13:20:59 +0530

    made organizer funstion for file system manager

--- FILE SYSTEM ORGANIZER/FO.js
+++ FILE SYSTEM ORGANIZER/FO.js
@@ -16,12 +16,35 @@
 
 let inputArr = process.argv.slice(2); // to take multiple iput we use slice it remoeves input before 2 index
 
+let types = {    //it is a object
+    media: ["mp4", "mkv", "mp3"],
+    archives: ["zip", "7z", "rar", "gz", "ar", "iso", "xz"],
+    documents: [
+        "docx",
+        "doc",
+        "pdf",
+        "xlsx",
+        "xls",
+        "odt",
+        "ods",
+        "odp",
+        "odg",
+        "odf",
+        "txt",
+        "ps",
+        "tex",
+    ],
+    app: ["exe", "dmg", "pkg", "deb"],
+};
+
+const { dir } = require("console");
+const fs = require("fs")  //importin fs module
+const path = require('path')  //importing path module
 //console.log(input)  
 
-let command=inputArr[0];  ///tree , help ,organize
+let command = inputArr[0];  ///tree , help ,organize
 
-switch(command)
-{
+switch (command) {
     case 'tree':
         console.log('tree implemented')
         break;
@@ -32,7 +55,8 @@ switch(command)
         break;
 
     case 'organize':
-        console.log('organize command')
+        // console.log('organize command')
+        organizeFn(inputArr[1]);               //input to be given as node .\FO.js organize 'C:\DEV\test folder'
         break;
 
     default:
@@ -40,8 +64,7 @@ switch(command)
         break;
 }
 
-function helpfn()
-{
+function helpfn() {
     console.log(`List of all Commmand-
                 1)Tree command- node .\FO.js tree <dirName>
 
@@ -50,3 +73,96 @@ function helpfn()
                 3)Help command- node .\FO.js help
     `)
 }
+
+function organizeFn(dirPath) {
+    let destPath;    //globally declaring a destination path
+    //input of directory path
+    if (dirPath == undefined) {
+        console.log('Please enter a valid Directory path ');  //checking wether the path is passed or not
+        return;
+        // console.log(dirPath)  
+    }
+    else {
+        let doesExist = fs.existsSync(dirPath);
+        //  console.log(doesExist)
+        //wether the path is valid or not and it exist or not
+
+
+        if (doesExist == true) {
+            destPath = path.join(dirPath, 'organized_files');
+
+            // C:\DEV\test folder -this is dir path we join organized_files using join function of path module
+            //new path for folder -C:\DEV\test folder\organized_files
+
+            if (fs.existsSync(destPath) == false) {
+                fs.mkdirSync(destPath);  //making folder if folder does not exist
+
+            }
+            else {
+                console.log('This folder Already Exist')  // folder already existing 
+            }
+        }
+        else {
+            console.log('Enter valid path');
+        }
+    }
+    organizeHelper(dirPath, destPath);
+}
+
+//we are organizing this function to cataegorize our files
+function organizeHelper(src, dest)  //take source and destination 
+{
+    let childNames = fs.readdirSync(src);
+    // console.log(childNames) 
+    for (let i = 0; i < childNames.length; i++) {
+        let childAddress = path.join(src, childNames[i])  //path is identify of the files
+        let isFile = fs.lstatSync(childAddress).isFile(); //check here to only identify the files
+
+        // console.log(childAddress +" "+ isFile) 
+
+        if (isFile == true) {
+            let fileCategory = getCataegory(childNames[i])
+            // console.log(childNames[i] +" belongs to " +fileCategory)
+
+            sendFiles(childAddress, dest ,fileCategory)
+        }
+    }
+}
+
+function getCataegory(name) {
+    let ext = path.extname(name);   //taking out the extension of the files
+    // console.log(ext) 
+    ext = ext.slice(1) //removing dot from extension
+    // console.log(ext)
+
+    for (let type in types) {
+        let cTypeArr = types[type];
+        // console.log(cTypeArr)
+
+        for (let i = 0; i < cTypeArr.length; i++) {
+            if (ext == cTypeArr[i])
+            //match the values with the extensions
+            {
+                return type;
+            }
+        }
+    }
+    return 'others'
+}
+ function sendFiles(srcFilePath , dest , fileCategory)
+ {
+     let catPath= path.join(dest , fileCategory)  
+     if(fs.existsSync(catPath)==false)   //checking for cataegory folder path exist or not
+        {
+            fs.mkdirSync(catPath)
+        }
+
+      let fileName= path.basename(srcFilePath);   // we took out names of the files
+      let destFilePath=  path.join(catPath, fileName); //created path for files in cataegory folder
+
+      fs.copyFileSync(srcFilePath, destFilePath)   //copied the files
+      fs.unlinkSync(srcFilePath)  //deleted the files
+
+      console.log(fileName +"is copeid to" +fileCategory)
+ }
+
