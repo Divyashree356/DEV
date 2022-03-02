4892a59a71fe7a49504932918b0aeee7a6997a51
Divyashree356 <dshagrawal13@gmail.com>
2022-03-02 22:47:41 +0530

    convert json to excel

--- espnScrapper/usingAjsonFile.js
+++ espnScrapper/usingAjsonFile.js
@@ -1,15 +1,23 @@
 const fs= require('fs');
+const xlsx= require('xlsx');
+//without fs module
 
-let buffer = fs.readFileSync('./example.json')
+let jsonFile = require('./example.json')
+
+// let buffer = fs.readFileSync('./example.json')
+//above statement is not required it we use jsonfile format
 // console.log(buffer)
 
 
 //JSON is inbuilt in javascript method 
-let data= JSON.parse(buffer);
+// let data= JSON.parse(buffer);
+//this method is used to convert buffer type data or anyother type of data to json format
+//it is not required using  when using jsonFile
 
 // console.log(data)
 
-data.push(
+//pushing a  new object to our jsonfile
+jsonFile.push(
     {
         "name" : "thor" ,
         "last name" : "odinson" ,
@@ -25,10 +33,24 @@ data.push(
 
 // console.log(data);
 
-let strinData = JSON.stringify(data);
+let strinData = JSON.stringify(jsonFile);
+//converting data to string type so that we can add it to json because any file can be appended by only using a text file
 // console.log(strinData)
 
-fs.writeFileSync('example.json' , strinData);
+// fs.writeFileSync('example.json' , strinData);
+// console.log('json file updated')
 
-console.log('json file updated')
+        let  newWB = xlsx.utils.book_new();
+        //creating a new workbook
+        let newWS = xlsx.utils.json_to_sheet(jsonFile);
+        //json is converted to sheet format
+        xlsx.utils.book_append_sheet(newWB  , newWS , 'Avengers');
+        //add data to file and append together
+        xlsx.writeFile(newWB , 'Aven.xlsx');
+
+
+        let  wb= xlsx.readFile('Aven.xlsx');
+        let excelData = wb.Sheets['Avengers'];
+        let ans = xlsx.utils.sheet_to_json(excelData);
+        console.log(ans);
 
