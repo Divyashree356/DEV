6642f66f9f6ca71959d6ee524c664c86336f19a2
Divyashree356 <dshagrawal13@gmail.com>
2022-04-28 13:52:13 +0530

    push

--- To Do List/script.js
+++ To Do List/script.js
@@ -1,6 +1,20 @@
-let addbtn = document.querySelector('.toolbox-button');
-addbtn.addEventListener('click' , function()
+
+let addbtn = document.querySelector('.add-button');
+let addFlag= false;
+
+let modelContainer= document.querySelector('.model-cont')
+addbtn.addEventListener('click' , function(e)
 {
-    console.log('linked');
+    //display model 
+    //add fla ->true then we have to display model otherwise hide
+  addFlag = !addFlag;
+  if(addFlag)
+  {
+      modelContainer.style.display="flex";
+  }
+  else
+   modelContainer.style.display='none';
+
+
 })
 
