31b89aefc4ae6090a5927ad2c187c90de7087850
Divyashree356 <dshagrawal13@gmail.com>
2022-05-01 16:45:34 +0530

    adding script

--- To Do List/script.js
+++ To Do List/script.js
@@ -1,5 +1,6 @@
 
 let addbtn = document.querySelector('.add-button');
+let mainCont= document.querySelector('.main-cont');
 let addFlag= false;
 
 let modelContainer= document.querySelector('.model-cont')
@@ -14,7 +15,34 @@ addbtn.addEventListener('click' , function(e)
   }
   else
    modelContainer.style.display='none';
-
-
 })
+ //generate ticket
+ modelContainer.addEventListener('keydown' , function(e)
+ {
+     let key = e.key;
+     if(key == 'Shift')
+      {
+          createTicket(); //it will create ticket
+      modelContainer.style.display='none'
+      addFlag= false;
+        }
+ });
+
+ function createTicket()
+ {
+     let ticketCont= document.createElement('div');
+     ticketCont.setAttribute('class' , 'ticket-cont');
+     ticketCont.innerHTML=`   <div class="ticket-color">
+
+     </div>
+     <div class="ticket-id">
+
+     </div>
+     <div class="task-area">
+
+     </div>`
+
+     mainCont.appendChild(ticketCont);
+ }
+
 
